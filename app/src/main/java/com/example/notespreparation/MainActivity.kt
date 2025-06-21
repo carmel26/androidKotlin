79ea8a1b4735d3621da487223459495712b7af08
carmel26 <nkeshipelagie@gmail.com>
2025-06-21 13:10:16 +0300

    normal menu Items

--- app/src/main/java/com/example/notespreparation/MainActivity.kt
+++ app/src/main/java/com/example/notespreparation/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.notespreparation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item1 -> {
+                Toast.makeText(applicationContext, "Item 1 Selected", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.item2 -> {
+                Toast.makeText(applicationContext, "Item 2 Selected", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.item3 -> {
+                Toast.makeText(applicationContext, "Item 3 Selected", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
